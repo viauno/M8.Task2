37f7ee29604066294722e77523afacf38205eb20
viauno <viauno51@gmail.com>
2023-03-30 01:20:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Runtime.Intrinsics.Arm;
+
+namespace M8.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long weight = 0;
+            string DirPath = "";
+            do
+            {
+                Console.WriteLine("Введите путь до папки для очистки:");
+                DirPath = @"" + Console.ReadLine();
+            } while (CheckDir(DirPath));
+
+            WeighDir(DirPath,ref weight);
+
+            Console.WriteLine($"Размер заданой папки = {weight} байт");
+        }
+
+        static bool CheckDir(string dp)
+        {
+            if (dp != "")
+            {
+                if (Directory.Exists(dp))
+                {
+                    return false;
+                }
+                else { Console.WriteLine("Введенная папка не существует"); return true; }
+            }
+            else { return true; };
+        }
+
+        static void WeighDir(string dp, ref long weight)
+        {
+            foreach (var d in Directory.GetDirectories(dp))
+            {
+                WeighDir(d,ref weight);
+            }
+
+            foreach (var f in Directory.GetFiles(dp))
+            {
+                long length = new System.IO.FileInfo(f).Length;
+                weight += length;
+            }
+            return;
+        }
+
+    }
+}
